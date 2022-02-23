e344f14cef3eae1bbcc7f3d54df10b29965d7d80
Tomisin Oluwasile <75077076+tomisile@users.noreply.github.com>
2022-02-23 14:54:17 +0100

    Update streamlit app (#31)
    * update streamlit app

--- apache_app.py
+++ apache_app.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 import time
+import warnings
+warnings.filterwarnings('ignore')
 
 
 # App title
@@ -12,8 +14,8 @@ data_url = ('https://raw.githubusercontent.com/apacheteam/Crime-Against-Women-PP
 #data_url = (r'C:\Users\Tomi\datasets\crimes_against_women_2001-2014.csv')
 
 @st.cache
-def load_data(nrows):
-    data = pd.read_csv(data_url, nrows=nrows)
+def load_data():
+    data = pd.read_csv(data_url)
     return data
 
 # Let the reader know the data is loading.
@@ -21,8 +23,8 @@ data_load_state = st.text('Loading data...')
 
 # Load data
 st.subheader('Select data size')
-rows_to_load = st.slider('Number of samples (rows)', 1000, 10677, 5000)
-dataset = load_data(rows_to_load)
+#rows_to_load = st.slider('Number of samples (rows)', 1000, 10677, 5000)
+dataset = load_data()
 
 # Notify the reader that the data was successfully loaded.
 data_load_state.text("Loading data...done! (using st.cache)")
@@ -32,13 +34,14 @@ if st.checkbox('Show Analysis'):
     # Inspect the raw data
     st.subheader('Raw data')
     st.write(dataset)
+    st.write(dataset.shape)
 
     # Show rape cases per year
     st.subheader('Number of rape cases per year')
-    #hist_values = np.histogram(dataset[''])
-    #st.bar_chart(hist_values)
+    # hist_values = np.histogram(dataset[''])
+    # st.bar_chart(hist_values)
 
-    # plot a line chart for rape cases
+    # plot a line chart for crime cases per year
     rape_data = pd.DataFrame(dataset.groupby('year')['rape'].sum())
     st.line_chart(rape_data['rape'])
 
@@ -59,8 +62,3 @@ option2 = st.sidebar.selectbox(
 'Predict number of ', option1, 'in the year ', str(option2)
 
 st.button('PREDICT')
-
-
-print('Debug: Complete')
-
-

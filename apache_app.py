31ff630f06990d110f8d60e78693e6844a3175ae
Tomisin Oluwasile <75077076+tomisile@users.noreply.github.com>
2022-02-23 20:20:12 +0100

    Update apache_app.py

--- apache_app.py
+++ apache_app.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import sklearn
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 import warnings
